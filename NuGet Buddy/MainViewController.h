72da4a6e988b604ef7a7f291d85436e257850267
moozzyk <moozzyk@gmail.com>
2016-07-20 18:43:57 -0700

    Adding UI for specifying feeds

---
+++ NuGet Buddy/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  NuGet Buddy
+//
+//  Created by Pawel Kadluczka on 7/20/16.
+//  Copyright © 2016 Pawel Kadluczka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainViewController : NSViewController
+
+@end
---
+++ NuGet Buddy/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  NuGet Buddy
+//
+//  Created by Pawel Kadluczka on 7/20/16.
+//  Copyright © 2016 Pawel Kadluczka. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
