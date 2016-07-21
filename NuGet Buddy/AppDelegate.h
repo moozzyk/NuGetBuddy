6a33b0248effa29163ebfef55256f7a867887d0f
moozzyk <moozzyk@gmail.com>
2016-07-20 18:22:34 -0700

    Initial Commit

---
+++ NuGet Buddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NuGet Buddy
+//
+//  Created by Pawel Kadluczka on 7/20/16.
+//  Copyright © 2016 Pawel Kadluczka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NuGet Buddy/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NuGet Buddy
+//
+//  Created by Pawel Kadluczka on 7/20/16.
+//  Copyright © 2016 Pawel Kadluczka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
